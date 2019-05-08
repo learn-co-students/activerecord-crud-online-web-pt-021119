b55f79cd766da2e1c76e60efd7e3b2c83a94a61c
github username <github email address>
2019-05-08 15:20:14 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,19 +22,20 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block( attributes = {title: "Home Alone",
+release_date: 1990})
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = attributes[:title]
+    m.release_date = attributes[:release_date]
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -42,7 +43,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 1).title
+  Movie.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_05_07_215325) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
