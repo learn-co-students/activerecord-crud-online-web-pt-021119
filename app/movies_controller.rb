8fcad8df827d77d4ac4aca33978de4cff9dbf7d5
Eyal Toledano <eutait@gmail.com>
2019-05-08 13:06:48 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,7 +74,9 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.last(5) do |movie|
+    binding.pry
     movie.update(title: "A Movie")
+
   end
 end
 
