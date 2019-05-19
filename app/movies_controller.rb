cd12b7eadd2d47c0b8571508ac843fe67a0a069e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-05-19 08:57:22 +0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,20 +31,19 @@ def can_be_created_in_a_block(args={title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  all_movies = Movie.all
-  first_item = all_movies.first
+  Movie.all.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.all.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
@@ -52,21 +51,21 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where(release_date: 2002)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  awesome = Movie.find_by(title: "Awesome Flick")
+  awesome.update(title: "Even Awesomer Flick")
+  awesome.save
 end
 
 def can_update_using_update_method
